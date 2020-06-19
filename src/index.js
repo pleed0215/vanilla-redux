4ef676544815623d10bb82d547101de681216e60
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-06-19 12:12:04 +0900

    fourth commit

--- src/index.js
+++ src/index.js
@@ -9,48 +9,82 @@ ReactDOM.render(
   document.getElementById("root")
 );*/
 import { createStore } from "redux";
+let globalIndex = 0;
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const number = document.getElementById("number");
+const formTodo = document.getElementById("form-todo");
+const todoText = document.getElementById("todo-input");
+const submit = document.getElementById("submit-todo");
+const todoUl = document.getElementById("todo-list");
 
-const countModifier = (state = 0, action) => {
+const ITEM_ADD = "@TODO_ITEM_ADD";
+const ITEM_REMOVE = "@TODO_ITEM_REMOVE";
+
+const todoModifier = (todoItems = [], action) => {
+  const item = action?.data?.item,
+    id = action?.data?.id;
+
   switch (action.type) {
-    case "PLUS":
-      return state + 1;
-    case "MINUS":
-      return state - 1;
+    case ITEM_ADD:
+      return [...todoItems, { id, item }];
+    case ITEM_REMOVE:
+      return todoItems.filter((todo) => todo.id !== action?.data?.id);
     default:
-      return state;
+      return todoItems;
   }
 };
-const countStore = createStore(countModifier);
-countStore.subscribe(() => {
-  number.innerHTML = countStore.getState();
-});
 
-console.log(countStore.getState());
+const todoStore = createStore(todoModifier);
+const todosRender = () => {
+  const todos = todoStore.getState();
 
-plus.addEventListener("click", () => {
-  countStore.dispatch({ type: "PLUS" });
-});
+  while (todoUl.hasChildNodes()) {
+    todoUl.removeChild(todoUl.firstChild);
+  }
 
-minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
-});
+  if (todos && todos.length > 0) {
+    todos.map((todo) => {
+      const node = document.createElement("li");
+      const textNode = document.createTextNode(todo.item);
+      const buttonNode = document.createElement("button");
 
-/*
-let count = 0;
-const updateNumber = () => {
-  number.innerHTML = count;
+      node.id = `@item-${todo.id}`;
+      buttonNode.innerText = "𝗫";
+      buttonNode.style.margin = "2px 0px 2px 15px";
+      buttonNode.addEventListener("click", (e) => {
+        console.log(e);
+        const pickedId = parseInt(e.target.parentNode.id.split("-")[1]);
+
+        todoUl.removeChild(e.target.parentNode);
+        console.log(e.target.parentNode);
+        todoStore.dispatch({
+          type: ITEM_REMOVE,
+          data: {
+            id: pickedId,
+          },
+        });
+      });
+      node.appendChild(textNode);
+      node.appendChild(buttonNode);
+      todoUl.appendChild(node);
+    });
+  }
 };
 
-plus.addEventListener("click", () => {
-  count++;
-  updateNumber();
-});
+todoStore.subscribe(todosRender);
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
 
-minus.addEventListener("click", () => {
-  count--;
-  updateNumber();
-});*/
+  if (todoText.value) {
+    const item = todoText.value;
+    const id = globalIndex++;
+
+    todoStore.dispatch({
+      type: ITEM_ADD,
+      data: {
+        id,
+        item,
+      },
+    });
+  }
+  todoText.value = "";
+});
